686eb3dd6b444944598d731a69b5df3687224a94
exihub <exibroker@gmail.com>
2024-03-30 08:48:04 +0700

    a

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log('start')
+const { readdir, readFile, open } = require('node:fs/promises');
+const { readFileSync, writeFile } = require('fs')
+var fs = require('fs');
+async function convertTextToJson() {
+    const srcPath = __dirname + '/source';
+    const descPath = __dirname + '/destination';
+    const files = await readdir(__dirname + '/source');
+    const keyLenght = +(process.env.KEY_LENGTH || 40)
+    for await (const path of files) {
+        console.log(path);
+        // const data = fs.readFileSync(srcPath + '/' + path);
+        // // const lines = await file.readLines();
+        // console.log(`lines`, data);
+        if (path.endsWith('.txt')) {
+            const file = await open(srcPath + '/' + path);
+            const textJson = {};
+            for await (const line of file.readLines()) {
+                console.log(line);
+                const tmp = line.split('.')[0]
+                let key = tmp.split(' ').join('_').toLowerCase();
+                if (key.length > keyLenght) {
+
+                    key = key.slice(0, keyLenght);
+                }
+                textJson[key] = line;
+            }
+            writeFile(descPath + '/' + path.replace('.txt', '.json'), JSON.stringify(textJson), (error) => {
+                console.log(`error`, error);
+            })
+        } else {
+            console.log(`${path} not text file`);
+        }
+
+    }
+
+}
+
+convertTextToJson();
