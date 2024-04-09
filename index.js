ac685ac8e1a46e744680c4b757db6ed049424ee0
exihub <exibroker@gmail.com>
2024-04-09 19:27:43 +0700

    a

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 console.log('start')
 const { readdir, readFile, open } = require('node:fs/promises');
 const { readFileSync, writeFile } = require('fs')
-var fs = require('fs');
+require('dotenv').config();
+
 async function convertTextToJson() {
+    console.log(`[convertTextToJson] call`);
     const srcPath = __dirname + '/source';
     const descPath = __dirname + '/destination';
     const files = await readdir(__dirname + '/source');
@@ -17,7 +19,7 @@ async function convertTextToJson() {
             const textJson = {};
             for await (const line of file.readLines()) {
                 console.log(line);
-                const tmp = line.split('.')[0]
+                const tmp = line.split('.')[0].replace(/[-.'?#!%&,]/g, '')
                 let key = tmp.split(' ').join('_').toLowerCase();
                 if (key.length > keyLenght) {
 
@@ -25,15 +27,15 @@ async function convertTextToJson() {
                 }
                 textJson[key] = line;
             }
-            writeFile(descPath + '/' + path.replace('.txt', '.json'), JSON.stringify(textJson), (error) => {
-                console.log(`error`, error);
+            writeFile(descPath + '/' + path.replace('.txt', '.json'), JSON.stringify(textJson), (value) => {
+                console.log(`writeFile`, value);
             })
         } else {
             console.log(`${path} not text file`);
         }
 
     }
-
+    console.log(`[convertTextToJson] done`);
 }
 
 convertTextToJson();
